e56e6fc5ff9ae7d8f9eab6cce153ad04d61f9373
Nathan Welliver <nwelliver.564@gmail.com>
2024-04-25 15:16:36 -0400

    complete lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+
+    function good(){
+        console.log('Good!')
+    }
+
+    function hey(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+            <button onFocus={good} onBlur={hey}>'Eyes on me'</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+    function logging(){
+        console.log('Entering password...')
+    }
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={logging}/>
+        </div>
     )
 }
 
